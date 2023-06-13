e7024fc6682fb09d427096d223b8bb609f1620de
Felipe Castro <felipe.castro.castillo@gmail.com>
2023-06-13 11:04:20 -0400

    mensaje 3

--- cicloforicecream.js
+++ cicloforicecream.js
@@ -2,27 +2,29 @@
 
 
 let iceCreamFlavors = [
-    "Chocolate",
-    "Strawberry",
-    "Vanilla",
-    "Pistacho",
-    "Neapolitan",
-    "Mint Chip",
-  ];
-  
-  
-  let iWantAIceCream = window.prompt("Que sabor de helado deseas?", "");
-  let iceCreamFound;
-  for (let i = 0; i < iceCreamFlavors.length; i++) {
-    if (iceCreamFlavors[i].toLowerCase() === iWantAIceCream.toLowerCase()) {
-      iceCreamFound = iceCreamFlavors[i];
-    }
+  "Chocolate",
+  "Strawberry",
+  "Vanilla",
+  "Pistacho",
+  "Neapolitan",
+  "Mint Chip",
+  "Fantasía",
+  "Cafe",
+  "chocolate2"
+];
+
+
+let iWantAIceCream = window.prompt("Que sabor de helado deseas?", "");
+let iceCreamFound;
+for (let i = 0; i < iceCreamFlavors.length; i++) {
+  if (iceCreamFlavors[i].toLowerCase() === iWantAIceCream.toLowerCase()) {
+    iceCreamFound = iceCreamFlavors[i];
   }
-  if(iWantAIceCream === "") {
-     console.log("No elegiste un sabor de helado");
-  } else if (iceCreamFound) {
-    console.log(`Aquí está tu helado de ${iceCreamFound}, disfrútalo!`);
-  } else {
-    console.log(`Lo siento, no tenemos helados sabor ${iWantAIceCream}`);
-  }
-  
+}
+if (iWantAIceCream === "") {
+  console.log("No elegiste un sabor de helado");
+} else if (iceCreamFound) {
+  console.log(`Aquí está tu helado de ${iceCreamFound}, disfrútalo!`);
+} else {
+  console.log(`Lo siento, no tenemos helados sabor ${iWantAIceCream}`);
+}
